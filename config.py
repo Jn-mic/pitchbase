779f820cf9b26d374dce913a6c4bbf78020b9e46
jn-mic <jackotienokey@gmail.com>
2021-06-24 11:40:28 +0300

    creating a database

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://eyrduqspatymez:3f343f88ab24a646106266360032b3636c0c304eee5eba3c8e0c4d706068ed00@ec2-54-224-194-214.compute-1.amazonaws.com:5432/dc91pqn3gimkq5'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #Email configuration
+    MAIL_SERVER= 'smtp.googlemail.com'
+    MAIL_PORT = '587'
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
