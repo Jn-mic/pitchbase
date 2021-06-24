a351160c37b4059527bac5763a6d8aa0bac6fa0e
jn-mic <jackotienokey@gmail.com>
2021-06-24 15:02:05 +0300

    creating app instances on manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
+
+from app import create_app, db
+from app.models import Comment, Pitch, User
+
+# Creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Pitch = Pitch, Comment = Comment)
+if __name__ == '__main__':
+    manager.run()
